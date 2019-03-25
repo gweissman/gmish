fba055dd8e35a1775726266725a668ec19ed90a9
Gary Weissman <632713+gweissman@users.noreply.github.com>
2019-03-25 09:05:34 -0500

    Added documentation for scaled brier score

--- R/sbs.R
+++ R/sbs.R
@@ -1,7 +1,16 @@
-# Scaled Brier Score
-#
-# This function calculates the Scaled Brier Score for predicted probabilities against a binary outcome.
-
+#' Calculate the Scaled Brier Score for predicted probabilities against a binary outcome.
+#'
+#' @param preds A vector of predicted probabilities.
+#' @param obs A vector containing the observed binary outcomes (0 or 1).
+#' @return The Scaled Brier Score given by \deqn{ BS_{scaled} = 1 - \frac{BS}{BS_{max}}} where
+#' \deqn{BS_{max} = \frac{1}{N} \sum (y - \bar y)^2}.
+#' @examples
+#' # Generate some predictions
+#' predictions <- runif(1000)
+#' # Generate some binary outcomes
+#' observations <- sample(0:1, size = 1000, replace = TRUE)
+#' # Calculate the Scaled Brier Score
+#' sbs(predictions, observations)
 sbs <- function(preds, obs) {
   1 - (bs(preds, obs)) / (bs(mean(obs), obs))
 }
