4d9d02d91765e798c855ad2b155961308cf2e7f7
Gary Weissman <632713+gweissman@users.noreply.github.com>
2020-02-11 16:56:21 -0500

    Warnings to account for ent metric

--- R/bs_ci.R
+++ R/bs_ci.R
@@ -23,7 +23,10 @@ bs_ci <- function(preds, obs, metric = NULL, reps = 1000, conf = 0.95,
   assertthat::assert_that(is.function(metric), msg = 'metric must be of the form function(preds, obs)')
   assertthat::assert_that(length(preds) == length(obs), msg = 'preds and obs must be of equal length')
   assertthat::assert_that(all(preds>=0) & all(preds<=1), msg = 'all values of preds must fall between 0 and 1 (inclusive)')
-  assertthat::assert_that(all(obs %in% c(0,1)), msg = 'all values of obs must be 0 or 1')
+  if (metric != 'ent') {
+    assertthat::assert_that(all(obs %in% c(0,1)), msg = 'all values of obs must be 0 or 1')
+  }
+  if (metric == 'ent' && ! is.null(obs)) warning('Variable obs is ignored for entropy calculation.')
   # Seed
   if (! is.null(seed)) set.seed(seed)
 
