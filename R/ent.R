83cf426d7aae74a2654281ba9335851f1ebc7957
Gary Weissman <632713+gweissman@users.noreply.github.com>
2020-02-11 16:53:19 -0500

    Added new function to calculate entropy

---
+++ R/ent.R
@@ -0,0 +1,17 @@
+#' Calculate the mean entropy for the predicted probabilities over an arbitrary set of classes.
+#' @export
+#'
+#' @param preds A vector, matrix, or data frame of predicted probabilities. Each column should contain probabilities for a unique class.
+#' @return The mean entropy over all predictions given by \deqn{ E = \frac{1}{N} \sum -\sum (p log(p))}
+#' @examples
+#' # Generate some predictions
+#' predictions <- runif(1000)
+#' # Generate some binary outcomes
+#' observations <- sample(0:1, size = 1000, replace = TRUE)
+#' # Calculate the mean entropy
+#' ent(predictions)
+ent <- function(preds) {
+  mean(
+    apply(preds, 1, function(p) -sum(p * log(p)))
+  )
+}
